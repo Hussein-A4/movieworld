61a8891be96377dea9cf81a535882c1403cfeba4
Hussein Al-Habbobi <smartron4cr7@gmail.com>
2022-12-16 14:18:44 -0500

    First commit

---
+++ src/views/homeView.vue
@@ -0,0 +1,21 @@
+<script setup>
+import Top from "../components/header.vue"
+import middle from "../components/hero.vue"
+import bottom from "../components/footer.vue"
+</script>
+
+<template>
+<div class="header">
+    <Top />
+</div>
+<div class="hero">
+    <middle />
+</div>
+<div class="footer">
+    <bottom />
+</div>
+</template>
+
+<style scoped>
+
+</style>
